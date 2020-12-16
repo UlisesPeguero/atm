fb95c69b009ff629be2cf7f075c7be56adcfe8dc
UlisesPeguero <peguero.ulises@gmail.com>
2020-12-15 23:49:14 -0600

    Framework set, working on Alerts

--- js/account.js
+++ js/account.js
@@ -1,16 +1,57 @@
+/*
+    Holds and controls the balance actions
+    +constructor
+    @param  intialData  {object}    -> starting values for the account
+*/
 function Account(initialData) {
     // private attributes
     let pin, name, balance;
 
     /*
         Initializes Account instance and set the starting values for pin, name and balance
-        @param  data   {Object} -> {pin, name, balance} starting values for the Account
+        @param  data   {Object}     -> {pin, name, balance} starting values for the Account
     */
     function init(data) {
         pin = data.pin;
         name = data.name;
         balance = data.balance || 0.00; // if not balance is provided we intialize on 0.00
     }
+
+    /*
+        Check if the deposit is a valid ammount and deposits it, returns true if so or false if is not
+        @param deposit  {number}    -> Ammount to be deposited in the balance
+        @return         {boolean}   -> flag that indicates if the deposit happened
+    */
+    this.deposit = deposit => {
+        if(deposit < 0) {
+            return false;
+        }
+        balance += deposit;
+        return true;
+    };
+
+    /*
+        Check if the withdraw is a valid ammount(no overdraft) and withdraws it,returns true if so or false if is not
+        @param deposit  {number}    -> Ammount to be withdrawn from the balance
+        @return         {boolean}   -> flag that indicates if the withdrawal happened
+    */
+    this.withdraw = withdrawal => {
+        if(balance - withdrawal < 0) {
+            return false;
+        }
+        balance += withdrawal;
+        return true;
+    };
+
+    //  @return     {number}    -> Returns the current balance in the account
+    this.getBalance = () => balance;
+    
+    //  @return     {object}    -> Returns the data in the account in JSON format
+    this.getJSON = () => ({
+        pin: pin,
+        name: name,
+        balance: balance.toFixed(2) // set the decimal precision to 2 
+    });
 
     init(initialData);
 }   
--- js/atm.js
+++ js/atm.js
@@ -1,12 +1,40 @@
+// control the application UI and data management
 function Atm() {
     // constants
+    // balance actions
+    const NONE = 0;
+    const SHOW = 1;
+    const DEPOSIT = 2;
+    const WITHDRAW = 3;
+    // Alerts controller
+    this.Alert = {
+        template: document.getElementById('alertTemplate'),
+        delay: 5 * 1000,    // 5 seconds
+        error(view, message) {  // shortcut to show an error alert
+            this.show(view, message, 'alert-danger');
+        },
+        success(view, message) {  // shortcut to show a success alert 
+            this.show(view, message, 'alert-success');
+        },  
+        /*
+            creates alert and sets the timeout
+            @param  view    {ViewContainer} -> View container where the alert will be shown
+            @param  message {string}        -> Message that will be displayed in the alert
+            @param  className {string}      -> Classes used by bootstrap to set the style
+            @param  delay   {number}  default this.delay -> Delay before the alert disappears, 
+        */
+        show(view, message, className, delay = this.delay) {
+            //let alert = this.template.content;
+            //view.get('form').appendChild(alert);
+        }
+    };
     // View will hold all the DOM for the UI views
     const View = {
-        login: document.getElementById('login'), // home screen
-        new: document.getElementById('newAccount'), // new account creation 
-        account: document.getElementById('account'), // account menu
-        balance: document.getElementById('balance'), // perform any action related to the balance (get, deposit, withdraw)
-        changePin: document.getElementById('changePin') // change current pin
+        login: new ViewContainer('loginView'), // home screen
+        new: new ViewContainer('newAccountView'), // new account creation 
+        account: new ViewContainer('accountView'), // account menu
+        balance: new ViewContainer('balanceView'), // perform any action related to the balance (get, deposit, withdraw)
+        changePIN: new ViewContainer('changePinView') // change current pin
     };
 
     // controls
@@ -14,58 +42,162 @@ function Atm() {
         login: document.getElementById('loginButton'),
         new: document.getElementById('newAccountButton'),
         balance: document.getElementById('balanceButton'),
-        changePin: document.getElementById('changePinButton'),
+        changePIN: document.getElementById('changePinButton'),
         deposit: document.getElementById('depositButton'),
         withdraw: document.getElementById('withdrawButton'),
         logout: document.getElementById('logoutButton')        
-    }
+    };
+
+    // attributes
+    let accounts = [];
+    // current values being used
+    let current = {        
+        view: null,
+        account: null,
+        action: NONE
+    };
+    // previous values, using an object for consistency with current
+    let previous = {
+        view: null
+    };
 
     // initializes application and UI
     function init() {
         // prepare related controls
         prepareControls();
         // initializes the UI to show the login view
-        showView(View.login);
+        openView(View.login);
     }
 
     // assigns control DOM elements and attaches events
     function prepareControls() {
+        // login view
         Button.login.onclick = login;
         Button.new.onclick = openNewAccount;
+        // new account view
+        View.new.setButtons({
+            accept: createAccount,
+            cancel: goBack
+        });
+        // account menu view
+        Button.changePIN.onclick = openChangePIN;
+        Button.balance.onclick = () => openBalance(SHOW);
+        Button.deposit.onclick = () => openBalance(DEPOSIT);
+        Button.withdraw.onclick = () => openBalance(WITHDRAW);
         Button.logout.onclick = logout;
+        // balance form view
+        View.balance.setButtons({
+            accept: updateBalance,
+            cancel: goBack
+        });
+        // change pin view
+        View.changePIN.setButtons({
+            accept: updatePIN,
+            cancel: goBack
+        });
     }
 
     // attemps to login by looking for the PIN provided
     function login() {
 
-        showView(View.account);
+        openView(View.account);
     }
 
     // closes current session and returns to login screen
     function logout() {
-        showView(View.login);
+        current.account = null; // removes the currently open account 
+        openView(View.login);
     }
 
     // open view to create new account
     function openNewAccount() {
-        showView(View.new);
+        openView(View.new);
+    }
+
+    function createAccount() {
+
     }
 
     /*
-        Hides all the the containers that are currently visible and displays the specified one
-        @param  container   {DOM} -> DOM that holds the container to be shown
+        Displays balance view for showing, deposit or withdraw
+        @param action   {number} default SHOW -> Identify the action we will be providing on the view
     */
-    function showView(view) {
-
-        document.querySelectorAll('.myContainer[visible]').forEach(dom => {
-            dom.removeAttribute('visible');
-            dom.style.display = 'none';
-        });
-        view.setAttribute('visible', true);        
-        view.style.display = 'block';
+    function openBalance(action = SHOW) {
+        current.action = action;
+        let title = ''; // will let us change the title of the view accordingly
+        let readOnly = false; // for deposit and withdraw we change the input to writable                            
+        switch(current.action) {
+            case SHOW:      title = 'Balance'; 
+                            // for show we change the input to readonly
+                            readOnly = true;
+                            break;
+            case DEPOSIT:   title = 'Deposit';
+                            break;
+            case WITHDRAW:  title = 'Withdraw';
+                            break;
+        }
+        // set title of the view
+        View.balance.get('#balance').readOnly = readOnly;
+        View.balance.setText('#title', title);
+        openView(View.balance);
     }
 
-    // calls initializing method
+    // update balance of the current account 
+    function updateBalance() {
+        let value = View.balance.get('#balance').value;
+        switch(current.action) {                    
+            case SHOW:      break; // do nothing
+            case DEPOSIT:   if(!current.account.deposit(value)) { // attempt to deposit, if works continue
+                                // if doesnt work
+                                
+                            }
+                            break;
+            case WITHDRAW:      
+                            break;
+        }        
+        current.action = NONE;
+        goBack();
+    }
+
+    function openChangePIN() {
+        openView(View.changePIN);
+    }
+
+    function updatePIN() {
+
+    }
+
+    function updateData() {
+
+    }
+
+    function loadData() {
+
+    }
+
+    // hides current view and goes back to the previous View
+    function goBack() {
+        if(previous.view !== null) {
+           openView(previous.view);
+        }
+    }
+
+    /*
+        Hides the view containers that is currently visible and displays the specified one
+        @param  container   {ViewContainer} -> Object that holds the container to be shown
+    */
+    function openView(view) {
+        if(current.view !== null) {
+            previous.view = current.view;
+            current.view.close(); // closes current view
+        }
+        current.view = view;
+        current.view.open(); // display requested view
+    }
+
+    this.views = () => View;
+
     init();
     console.log('ATM Ready.');    
 }
+
